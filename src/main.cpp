50ac0f12cd82a17367743d9bed750f104f650ff8
Will Castlen <castlen.3@buckeyemail.osu>
2026-03-04 20:11:25 -0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5,10 +5,9 @@
 #include <FEHMotor.h>
 
 DigitalEncoder right_encoder(FEHIO::Pin8);
-DigitalEncoder left_encoder(FEHIO::Pin9);
+DigitalEncoder left_encoder(FEHIO::Pin10);
 FEHMotor right_motor(FEHMotor::Motor0,9.0);
-FEHMotor left_motor(FEHMotor::Motor1,9.0);
-
+FEHMotor left_motor(FEHMotor::Motor2,9.0);
 
 void move_forward(int percent, int counts) //using encoders
 {
@@ -20,10 +19,28 @@ void move_forward(int percent, int counts) //using encoders
     left_motor.SetPercent(percent);
     //While the average of the left and right encoder is less than counts,
     //keep running motors
-    while((right_encoder.Counts()) < counts);
+    while((right_encoder.Counts() + left_encoder.Counts() /2) < counts);
 
     //Turn off motors
     right_motor.Stop();
+    left_motor.Stop();
+}
+
+void move_backward(int percent, int counts) //using encoders
+{
+    //Reset encoder counts
+    right_encoder.ResetCounts();
+    left_encoder.ResetCounts();
+    //Set both motors to desired percent
+    right_motor.SetPercent(-percent);
+    left_motor.SetPercent(-percent);
+    //While the average of the left and right encoder is less than counts,
+    //keep running motors
+    while((right_encoder.Counts() + left_encoder.Counts() /2) < counts);
+
+    //Turn off motors
+    right_motor.Stop();
+    left_motor.Stop();
 }
 
 void ERCMain()
@@ -32,10 +49,8 @@ void ERCMain()
     /////EDIT HERE/////
     ///////////////////
 
-    int inches = 6; //Input distance here
-    int motor_percent = 20; //Input power level here
-    int expected_counts = 40.489; //Input theoretical counts per inch of linear motion here
-
+ //Input distance here
+    int motor_percent = 40; //Input power level here (must be between 0 and 100
     int x, y; //for touch screen
 
     //Initialize the screen
@@ -46,21 +61,25 @@ void ERCMain()
     LCD.WriteLine("Touch the screen");
     while(!LCD.Touch(&x,&y)); //Wait for screen to be pressed
     while(LCD.Touch(&x,&y)); //Wait for screen to be unpressed
+    //drive 37 inches
+    int inches = 37;
+    motor_percent = 20;
+    int expected_counts = 40.489*inches;
+    move_forward(motor_percent, expected_counts);
+    Sleep(8.0); //Wait for counts to stabilize
 
-    //drive 6 inches
-    inches = 6;
+    //drive 2 feet
+    inches = 36;
+    motor_percent = 40;
     expected_counts = 40.489*inches;
     move_forward(motor_percent, expected_counts);
-
     Sleep(2.0); //Wait for counts to stabilize
-    //Print out data
-    LCD.Write("Theoretical Counts: ");
-    LCD.WriteLine(expected_counts);
-    LCD.Write("Motor Percent: ");
-    LCD.WriteLine(motor_percent);
-    LCD.Write("Actual Right Counts: ");
-    LCD.WriteLine(right_encoder.Counts());
-    LCD.WriteLine("Actual Left Counts: ");
-    LCD.WriteLine(left_encoder.Counts());
+    
+    //move back 2 feet
+    inches = 36;
+    motor_percent = 40;
+    expected_counts = 40.489*inches;
+    move_backward(motor_percent, expected_counts);
+    Sleep(2.0); //Wait for counts to stabilize
 
 }
